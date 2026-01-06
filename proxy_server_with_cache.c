3176cee62549cb79a8088e0e986037be4f1a9846
Nikhil Anand <nikhildfire8102@gmail.com>
2026-01-06 22:20:50 +0530

    Update proxy_server_with_cache.c

--- proxy_server_with_cache.c
+++ proxy_server_with_cache.c
@@ -2,6 +2,10 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include <pthread.h>
+#include <semaphore.h>
+
+#define MAX_CLIENTS 10
 
 typedef struct cache_element cache_element;
 
@@ -14,10 +18,14 @@ struct cache_element {
 };
 
 cache_element* find(char* url);
-int add_cache_element(char* data, int size, char* url)
+int add_cache_element(char* data, int size, char* url);
 void remove_cache_element();
 
 int port_number = 8080;
-int proxy
+int proxy_socketId;
+pthread_t tid[MAX_CLIENTS];
+sem_t semaphore;
+pthread_mutex_t lock;
+
 
 
